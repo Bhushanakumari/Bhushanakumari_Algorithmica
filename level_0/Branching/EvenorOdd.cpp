931db519ff805dc298b6183811ec0e537232a954
Bhushanakumari <94296189+Bhushanakumari@users.noreply.github.com>
2023-02-16 14:09:53 +0530

    EvenorOdd.cpp

---
+++ level_0/Branching/EvenorOdd.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	int n;
+	cout << "Enter the number: ";
+	cin >> n;
+	if(n % 2 == 0){
+		cout << "Even" << endl;
+	}
+	else{
+		cout << "Odd" << endl;
+	}
+	return 0;
+}
